cea726920c08161a1aeb504de34f781220ea5be8
monikka-24 <125337473+monikka-24@users.noreply.github.com>
2024-04-04 09:30:30 +0530

    Add files via upload

---
+++ ATM_interface.java
@@ -0,0 +1,135 @@
+import java.util.*;
+
+class ATM_Machine {
+    float balance;
+    int PIN = 1234;
+    Scanner sc = new Scanner(System.in);
+
+    public void checkPin() {
+        System.out.println("Enter your PIN: ");
+        int enteredPin = sc.nextInt();
+
+        if (enteredPin == PIN)
+            Menu();
+        else {
+            System.out.println("Invalid Pin");
+            checkPin();
+        }
+    }
+
+    public void Menu() {
+        System.out.println("----SERVICES----");
+        System.out.println("1. Check A/C Balance");
+        System.out.println("2. Deposit Money");
+        System.out.println("3. Withdraw Money");
+        System.out.println("4. Exit");
+
+        System.out.println("Enter your choice: ");
+        int n = sc.nextInt();
+        switch (n) {
+            case 1:
+                Check_Balance();
+                break;
+            case 2:
+                Deposit_Money();
+                break;
+            case 3:
+                Withdraw_Money();
+                break;
+            case 4:
+                System.out.println("-----THANKS FOR CONNECTING WITH US------");
+                System.exit(1);
+            default:
+                System.out.println("Please enter a valid option.");
+                System.out.println("*");
+                Menu();
+        }
+    }
+
+    public void Check_Balance() {
+        System.out.println("Your Balance: " + balance);
+        System.out.println("Enter 0 for exit OR press 1 for Menu");
+
+        int x = sc.nextInt();
+        if (x == 0) {
+            System.out.println("--THANKS FOR CONNECTING WITH US--");
+        }
+        if (x == 1) {
+            Menu();
+        } else {
+            System.out.println("Invalid Option!");
+        }
+    }
+
+    public void Deposit_Money() {
+        System.out.println("Enter the  amount you want to deposit: ");
+        int amt = sc.nextInt();
+
+        if (amt < 0) {
+            System.out.println("Enter 0 for exit OR press 1 for Menu");
+            int y = sc.nextInt();
+            if (y == 0) {
+                System.out.println("--THANKS FOR CONNECTING WITH US--");
+            }
+            if (y == 1) {
+                Menu();
+            } else {
+                System.out.println("Invalid Option!");
+            }
+        } else {
+            balance += amt;
+            System.out.println("Amount Deposited Successfully! Your new Balance is:" + balance);
+
+            System.out.println("Enter 0 for exit OR press 1 for Menu");
+            int cd = sc.nextInt();
+            if (cd == 0) {
+                System.out.println("--THANKS FOR CONNECTING WITH US--");
+            }
+            if (cd == 1) {
+                Menu();
+            } else {
+                System.out.println("Invalid Option!");
+            }
+        }
+    }
+
+    public void Withdraw_Money() {
+        System.out.println("Enter the amount to be withdrawed");
+        int withdraw_amt = sc.nextInt();
+
+        if (withdraw_amt < 0) {
+            System.out.println("Enter 0 for exit OR press 1 for Menu");
+            int z = sc.nextInt();
+            if (z == 0) {
+                System.out.println("--THANKS FOR CONNECTING WITH US--");
+            }
+            if (z == 1) {
+                Menu();
+            } else {
+                System.out.println("Invalid Option!");
+            }
+        } else {
+            balance -= withdraw_amt;
+            System.out.println("Amount Withdrawal Successfully! Your new Balance is:" + balance);
+
+            System.out.println("Enter 0 for exit OR press 1 for Menu");
+            int cw = sc.nextInt();
+            if (cw == 0) {
+                System.out.println("--THANKS FOR CONNECTING WITH US--");
+            }
+            if (cw == 1) {
+                Menu();
+            } else {
+                System.out.println("Invalid Option!");
+            }
+        }
+    }
+}
+
+public class ATM_interface {
+    public static void main(String args[]) {
+        System.out.println("---WELCOME---");
+        ATM_Machine ob = new ATM_Machine();
+        ob.checkPin();
+    }
+}
